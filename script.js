d8a439b9bd1b7089d71ed5b2ab02f64b66703d21
Diego Monti <diegmonti@gmail.com>
2019-06-18 21:04:10 +0200

    Add scripts

---
+++ script.js
@@ -0,0 +1,21 @@
+function displayTime() {
+    let now = new Date();
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+    m = formatNumber(m);
+    s = formatNumber(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(displayTime, 500);
+}
+
+function formatNumber(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+window.onload = function () {
+    displayTime();
+};
